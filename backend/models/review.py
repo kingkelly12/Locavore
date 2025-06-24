c40592f0cebc3373ad2555c804af154a43efbd1b
Kelly Koome <koomekelly7@gmail.com>
2025-06-24 10:36:12 +0300

    Add Review, User, and Vendor models to the backend

---
+++ backend/models/review.py
@@ -0,0 +1,11 @@
+from app import db
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendors.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
---
+++ backend/models/user.py
@@ -0,0 +1,19 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    vendors = db.relationship('Vendor', backref='owner', cascade="all, delete")
+    reviews = db.relationship('Review', backref='author', cascade="all, delete")
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
