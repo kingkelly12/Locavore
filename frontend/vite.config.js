4583be6c75d8c306d19ba00ce46dc7d30732499c
Kelly Koome <koomekelly7@gmail.com>
2025-06-24 19:06:07 +0300

    installed @vitejs/plugin-react

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,30 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { webcrypto } from 'crypto';
+
 if (!globalThis.crypto) {
   globalThis.crypto = webcrypto;
 }
+if (!globalThis.crypto.getRandomValues) {
+  globalThis.crypto.getRandomValues = webcrypto.getRandomValues.bind(webcrypto);
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+});
